2088c0d34b0b96b1fe60bda7851a72aae7cf96be
Ayash-Bera <ayashbera@gmail.com>
2024-12-03 21:27:58 +0530

    1212

--- vite.config.js
+++ vite.config.js
@@ -15,5 +15,25 @@ export default defineConfig({
         'process.env': {},
         global: 'window',
     },
-    base: "./",
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./src',
+                import.meta.url))
+        },
+        extensions: [
+            '.js',
+            '.json',
+            '.jsx',
+            '.mjs',
+            '.ts',
+            '.tsx',
+            '.vue',
+        ],
+    },
+    server: {
+        port: 3000,
+        open: true,
+        cors: true,
+    },
+    base: './',
 })
